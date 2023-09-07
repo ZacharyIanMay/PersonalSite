2c4d9f77d360b6572133c3c2ef8a6434de915c15
Zachary Ian May <78890982+ZacharyIanMay@users.noreply.github.com>
2023-09-07 14:36:34 -0700

    Added create new task function and button TODO: fix task table not refresshing properly

--- client/client/src/components/ToDo.vue
+++ client/client/src/components/ToDo.vue
@@ -1,29 +1,42 @@
 <template>
     <div>
         <h3>Welcome {{ username }}</h3>
+        
         <table class="center">
+
             <tr>
                 <th>Selection</th>
                 <th>Task ID</th>
                 <th>Task</th>
                 <th>Deadline</th>
             </tr>
+
             <tr v-for="(task, index) in tasks" :key="index">
                 <td><input type="checkbox" id="{{ index }}" /></td>
                 <td>{{task.taskid}}</td>
                 <td>{{task.task}}</td>
                 <td>{{new Date(task.deadline).toString()}}</td>
             </tr>
+
         </table>
+
+        <TaskInput v-if="newTask" @submitTask="createTask($event)" />
+
+        <button v-if="!newTask" id="button" @click="addTask()">Add a task</button>
     </div>
 </template>
 
 <script setup>
+    import TaskInput from './TaskInput.vue'
     import {defineProps, ref, watch} from 'vue';
     import axios from 'axios';
+
     const props = defineProps({user: String});
     const tasks = ref({});
     const username = ref(props.user);
+    const newTask = ref(0);
+    const update = ref(0);
+
     axios.post("http://localhost:3000/usertask", {username: username.value}).then(function (response) {
         //console.log(response);
         tasks.value = response.data;
@@ -43,7 +56,6 @@
         setTimeout(function() {
             username.value = props.user;
             axios.post("http://localhost:3000/usertask", {username: username.value}).then(function (response) {
-                console.log(response);
                 tasks.value = response.data;
                 if(tasks.value.success)
                 {
@@ -59,6 +71,50 @@
         }, 10)
     });
 
+    watch(update, () => {
+        setTimeout(function() {
+            username.value = props.user;
+            axios.post("http://localhost:3000/usertask", {username: username.value}).then(function (response) {
+                tasks.value = response.data;
+                if(tasks.value.success)
+                {
+                    tasks.value = tasks.value.tasks;
+                }
+                else
+                {
+                    tasks.value = [{taskid: 0, username: username, task: "No tasks", deadline:Date.now()}];
+                }
+            }).catch(function (error) {
+                console.log(`Error ${error}`);
+            });  
+        }, 10)
+    });
+
+    function addTask()
+    {
+        newTask.value = 1;
+    }
+
+    function createTask(event)
+    {
+        axios.post("http://localhost:3000/task", {username: username.value, task: event.task, deadline: event.deadline}).then(function () {
+                if(tasks.value.success)
+                {
+                    update.value = 1;
+                    setTimeout(function() {
+                        update.value = 0;
+                    }, 10)
+                }
+                else
+                {
+                    tasks.value = [{taskid: 0, username: username, task: "No tasks", deadline:Date.now()}];
+                }
+                newTask.value = 0;
+            }).catch(function (error) {
+                console.log(`Error ${error}`);
+            });
+    }
+
 </script>
 
 <style scoped>
@@ -70,6 +126,11 @@ p
 {
     padding-right: 10px;
     padding-left: 10px;
+}
+button
+{
+    padding: 2px;
+    margin: 10px;
 }
 .center
 {
