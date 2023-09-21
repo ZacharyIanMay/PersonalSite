1c8e981aa1eb78e7d654105723fd1fb9977354a8
Zachary Ian May <78890982+ZacharyIanMay@users.noreply.github.com>
2023-09-21 13:15:31 -0700

    Revert "Refactoring endpoints to enable testing via JEST."
    This reverts commit ba50594bbc93141a0c9b0d301c355b6f6ac6166d.

--- server/index.js
+++ server/index.js
@@ -8,226 +8,22 @@ const mysql = require('mysql')
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    let jwt = util.createJWT('test');
+    setTimeout(function() {
+        let result = util.isJWTValid(jwt);
+        let user = '';
 
+        util.getJWTUser(jwt);
 
-
-/**
- * Functions
- */
-module.exports
-{
-
-    function getBase(req, res)
-    {
-        let jwt = util.createJWT('test');
-        setTimeout(function() {
-            let result = util.isJWTValid(jwt);
-            let user = '';
-
-            util.getJWTUser(jwt);
-
-            res.send({"JWT": jwt, "User": user, "Result": result});
-        }, 1000)
-    }
-
-    function postJWT(req, res)
-    {
-        let jwt = req.body.jwt;
-        let user = util.getJWTUser(jwt);
-        let ret =
-        {
-            user: user
-        }
-        res.send(ret);
-    }
-
-    function postLogin(req, res)
-    {
-        let user = req.body.username;
-        let pass = req.body.password;
-        let conn = util.createConnection();
-        conn.query('select salt, pwHash from users where username = ?', user, (error, results, fields) => 
-        {
-            let ret = {
-                success: false,
-                username: user
-            }
-            if(!error && results.length === 1)
-            {
-                let salt = results[0].salt;
-                let pwHash = util.hash(pass, salt);
-                let verificationHash = results[0].pwHash;
-                if(pwHash === verificationHash)
-                {
-                    let jwt = util.createJWT(user);
-                    ret.success = true;
-                    ret.token = jwt;
-                }
-                else
-                {
-                    ret.error = "Incorrect Username or Password"
-                }
-                res.send(ret);
-            }
-            else
-            {
-                console.log(error);
-                ret.error =  "Incorrect Username or Password";
-                res.send(ret);
-            }
-        });
-        conn.end();
-    }
-
-    function postVerify(req, res)
-    {
-        let jwt = req.body.jwt;
-        let ret = {};
-        ret.valid = util.isJWTValid(jwt);
-        res.send(ret);
-    }
-
-    function postSignup(req, res)
-    {
-        let user = req.body.username;
-        let pass = req.body.password;
-        let salt = util.generateSalt();
-        let pwHash = util.hash(pass, salt);
-        /**
-         * Need to add a segment handling password hash retrieval
-         */
-        // Create connection
-        let conn = util.createConnection();
-        let err;
-        conn.query('select pwHash from users where username = ?', [user], (error, results, fields) => {
-            if(error) err = error;
-        });
-
-        let ret = {
-            success: false,
-            username: user
-        };
-
-        if(!err)
-        {
-            conn.query('insert into users value (?, ?, ?)', [user, pwHash, salt], (error, results, fields) => {
-                if(!error)
-                {
-                    let jwt = util.createJWT(user);
-                    ret.success = true;
-                    ret.token = jwt;
-                }
-                else
-                {
-                    ret.error = {code: error.code, errno: error.errno};
-                }
-                res.send(ret);
-            });
-            conn.end();
-        }
-        else
-        {
-            ret.error = err;
-            res.send(ret);
-        }
-    }
-
-    function postTask(req, res)
-    {
-        let user = req.body.username;
-        let task = req.body.task;
-        // deadline needs to be in yyyy-mm-dd hh:mm:ss
-        // TODO: add handling to enforce this format
-        let deadline = req.body.deadline;
-        // Create connection
-        let conn = util.createConnection();
-        // Form query
-        conn.query('insert into tasks (username, task, deadline) value (?, ?, ?)', [user, task, deadline], (error, results, fields) => {
-            let ret = {
-                success: false,
-                username: user
-            }
-            if(!error)
-            {
-                ret.success = true;
-            }
-            else
-            {
-                ret.error = {code: error.code, errno: error.errno};
-            }
-            res.send(ret);
-        });
-        conn.end();
-    }
-
-    function postDelete(req, res)
-    {
-        let user = req.body.username;
-        let taskid = req.body.taskid;
-        // Create connection
-        let conn = util.createConnection();
-        console.log(`delete from tasks where username = ${user} and taskid = ${taskid}`);
-        // Form query
-        conn.query('delete from tasks where username = ? and taskid = ?', [user, taskid], (error, results, fields) => {
-            let ret = {
-                success: false,
-                username: user
-            }
-            if(!error)
-            {
-                ret.success = true;
-            }
-            else
-            {
-                ret.error = {code: error.code, errno: error.errno};
-            }
-            res.send(ret);
-        });
-        conn.end();
-    }
-
-    function postUsertask(req, res)
-    {
-        let user = req.body.username;
-
-        let conn = util.createConnection();
-        // Form query
-        conn.query('select * from tasks where username = ?', user, (error, results, fields) => 
-        {
-            let ret = {
-                success: false,
-                username: user
-            }
-            if(error)
-            {
-                ret.error = error;
-            }
-            else if(results.length === 0)
-            {
-                ret.error = "No tasks";
-            }
-            else
-            {
-                ret.tasks = results;
-                ret.success = true;
-            }
-            res.send(ret);
-        });
-
-        conn.end();
-    }
-
-}
-
-/**
- * Endpoints
- */
+        res.send({"JWT": jwt, "User": user, "Result": result});
+    }, 1000)
+    
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`)
 })
-
-app.get('/', (req, res) => {getBase(req, res)})
 
 /**
  * Checks user credentials and issues a JWT login token if login is correct
@@ -236,17 +32,76 @@ app.get('/', (req, res) => {getBase(req, res)})
  * if failed res additionally contains error
  * if succesful res additionally contains token
  */
-app.post('/login', (req, res) => { postLogin(req, res)})
+app.post('/login', (req, res) => {
+    let user = req.body.username;
+    let pass = req.body.password;
+    let conn = util.createConnection();
+    conn.query('select salt, pwHash from users where username = ?', user, (error, results, fields) => 
+    {
+        let ret = {
+            success: false,
+            username: user
+        }
+        if(!error && results.length === 1)
+        {
+            let salt = results[0].salt;
+            let pwHash = util.hash(pass, salt);
+            let verificationHash = results[0].pwHash;
+            if(pwHash === verificationHash)
+            {
+                let jwt = util.createJWT(user);
+                ret.success = true;
+                ret.token = jwt;
+            }
+            else
+            {
+                ret.error = "Incorrect Username or Password"
+            }
+            res.send(ret);
+        }
+        else
+        {
+            console.log(error);
+            ret.error =  "Incorrect Username or Password";
+            res.send(ret);
+        }
+    });
+    conn.end();
+})
 
 /**
  * 
  */
-app.post('/jwt', (req, res) => {postJWT(req, res)})
+app.post('/jwt', (req, res) => {
+    let jwt = req.body.jwt;
+    let user = util.getJWTUser(jwt);
+    let ret =
+    {
+        user: user
+    }
+    res.send(ret);
+})
 
 /**
  * 
  */
-app.post('/verify', (req, res) => {postVerify(req, res)})
+app.post('/verify', (req, res) => {
+    let jwt = req.body.jwt;
+    let ret = {};
+    ret.valid = util.isJWTValid(jwt);
+    res.send(ret);
+})
+
+// app.post('/test', (req, res) => {
+//     let salt = req.body.salt;
+//     let pass = req.body.password;
+//     let hash = util.hash(pass, salt);
+//     console.log(hash);
+//     let ret = {
+//         hash: hash
+//     }
+//     res.send(ret);
+// })
 
 /**
  * creates a user account
@@ -254,7 +109,49 @@ app.post('/verify', (req, res) => {postVerify(req, res)})
  * res contains fields success and username
  * additionally, error if failed, or token is successful
  */
-app.post('/signup', (req, res) => {postSignup(req, res)})
+app.post('/signup', (req, res) => {
+    let user = req.body.username;
+    let pass = req.body.password;
+    let salt = util.generateSalt();
+    let pwHash = util.hash(pass, salt);
+    /**
+     * Need to add a segment handling password hash retrieval
+     */
+    // Create connection
+    let conn = util.createConnection();
+    let err;
+    conn.query('select pwHash from users where username = ?', [user], (error, results, fields) => {
+        if(error) err = error;
+    });
+
+    let ret = {
+        success: false,
+        username: user
+    };
+
+    if(!err)
+    {
+        conn.query('insert into users value (?, ?, ?)', [user, pwHash, salt], (error, results, fields) => {
+            if(!error)
+            {
+                let jwt = util.createJWT(user);
+                ret.success = true;
+                ret.token = jwt;
+            }
+            else
+            {
+                ret.error = {code: error.code, errno: error.errno};
+            }
+            res.send(ret);
+        });
+        conn.end();
+    }
+    else
+    {
+        ret.error = err;
+        res.send(ret);
+    }
+})
 
 /**
  * Adds a task to a given user
@@ -262,12 +159,60 @@ app.post('/signup', (req, res) => {postSignup(req, res)})
  * res contains fields success, username
  * additionally error if failed
  */
-app.post('/task', (req, res) => {postTask(req, res)})
+app.post('/task', (req, res) => {
+    let user = req.body.username;
+    let task = req.body.task;
+    // deadline needs to be in yyyy-mm-dd hh:mm:ss
+    // TODO: add handling to enforce this format
+    let deadline = req.body.deadline;
+    // Create connection
+    let conn = util.createConnection();
+    // Form query
+    conn.query('insert into tasks (username, task, deadline) value (?, ?, ?)', [user, task, deadline], (error, results, fields) => {
+        let ret = {
+            success: false,
+            username: user
+        }
+        if(!error)
+        {
+            ret.success = true;
+        }
+        else
+        {
+            ret.error = {code: error.code, errno: error.errno};
+        }
+        res.send(ret);
+    });
+    conn.end();
+})
 
 /**
  * 
  */
-app.post('/delete', (req, res) => {postDelete(req, res)})
+app.post('/delete', (req, res) => {
+    let user = req.body.username;
+    let taskid = req.body.taskid;
+    // Create connection
+    let conn = util.createConnection();
+    console.log(`delete from tasks where username = ${user} and taskid = ${taskid}`);
+    // Form query
+    conn.query('delete from tasks where username = ? and taskid = ?', [user, taskid], (error, results, fields) => {
+        let ret = {
+            success: false,
+            username: user
+        }
+        if(!error)
+        {
+            ret.success = true;
+        }
+        else
+        {
+            ret.error = {code: error.code, errno: error.errno};
+        }
+        res.send(ret);
+    });
+    conn.end();
+})
 
 /**
  * gets all the tasks for a given username
@@ -276,7 +221,35 @@ app.post('/delete', (req, res) => {postDelete(req, res)})
  * additionally if failed res contains field error
  * if succesful res contains field tasks
  */
-app.post('/usertask', (req, res) => {postUsertask(req, res)})
+app.post('/usertask', (req, res) => {
+    let user = req.body.username;
+
+    let conn = util.createConnection();
+    // Form query
+    conn.query('select * from tasks where username = ?', user, (error, results, fields) => 
+    {
+        let ret = {
+            success: false,
+            username: user
+        }
+        if(error)
+        {
+            ret.error = error;
+        }
+        else if(results.length === 0)
+        {
+            ret.error = "No tasks";
+        }
+        else
+        {
+            ret.tasks = results;
+            ret.success = true;
+        }
+        res.send(ret);
+    });
+
+    conn.end();
+})
 
 /**
  * gets all the tasks
@@ -285,34 +258,66 @@ app.post('/usertask', (req, res) => {postUsertask(req, res)})
  * additionally if failed res contains field error
  * if succesful res contains field tasks
  */
-// app.post('/task', (req, res) => {
-//     let user = req.body.username;
+app.post('/task', (req, res) => {
+    let user = req.body.username;
 
-//     let conn = util.createConnection();
-//     // Form query
-//     conn.query('select * from tasks', user, (error, results, fields) => 
-//     {
-//         let ret = {
-//             success: false
-//         }
-//         if(error)
-//         {
-//             ret.error = error;
-//         }
-//         else if(results.length === 0)
-//         {
-//             ret.error = "No tasks";
-//         }
-//         else
-//         {
-//             ret.tasks = results;
-//             ret.success = true;
-//         }
-//         res.send(ret);
-//     });
+    let conn = util.createConnection();
+    // Form query
+    conn.query('select * from tasks', user, (error, results, fields) => 
+    {
+        let ret = {
+            success: false
+        }
+        if(error)
+        {
+            ret.error = error;
+        }
+        else if(results.length === 0)
+        {
+            ret.error = "No tasks";
+        }
+        else
+        {
+            ret.tasks = results;
+            ret.success = true;
+        }
+        res.send(ret);
+    });
 
-//     conn.end();
-// })
+    conn.end();
+})
+
+/**
+ * Removes a task from a user's list of tasks given their username and the ID of the task to be deleted
+ * req requires fields username, taskid
+ * res contains fields success and username
+ * additionally if failed res contains field error
+ */
+app.delete('/task', (req, res) => {
+    let user = req.body.username;
+    let id = req.body.taskid;
+
+    let conn = util.createConnection();
+    // Form query
+    conn.query('delete from tasks where username = ? and taskid = ?', [user, id], (error, results, fields) => 
+    {
+        let ret = {
+            success: false,
+            username: user
+        }
+
+        if(error)
+        {
+            ret.error = error;
+        }
+        else
+        {
+            ret.success = true;
+        }
+        res.send(ret);
+    });
+    conn.end();
+})
 
 // app.post('/project', (req, res) => {
 
